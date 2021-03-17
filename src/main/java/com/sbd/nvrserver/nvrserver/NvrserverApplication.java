a1a2a45e21730002ca771323eba3226caeadcedd
xudaye2001 <xudaye@protonmail.com>
2021-03-17 10:56:07 +0800

    first commit

---
+++ src/main/java/com/sbd/nvrserver/nvrserver/NvrserverApplication.java
@@ -0,0 +1,13 @@
+package com.sbd.nvrserver.nvrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvrserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NvrserverApplication.class, args);
+    }
+
+}
